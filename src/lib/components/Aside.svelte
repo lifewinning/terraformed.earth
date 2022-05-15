dc4f321f93c9dcb32866a2aa599cb50d703ac030
Ingrid Burrington <lifewinning@gmail.com>
2022-05-15 11:40:00 -0400

    add transition for asides

--- src/lib/components/Aside.svelte
+++ src/lib/components/Aside.svelte
@@ -1,24 +1,13 @@
-<style>
-    aside{
-        transition: all 1s linear;
-        display: inline-grid;
-    }
-    .hidden{
-        height:0;
-        width: 0;
-        display: none;
-        transition: all 1s linear;
 
-    }
-</style>
 <script>
-    let active=true;
+    let active=false;
     export let label; 
     import { slide } from 'svelte/transition';
 </script>
 <label for='aside' on:click={() => {active = !active}}>{label}</label> 
 
-<aside class:hidden={active}>
+{#if active}
+<aside aria-live='polite' transition:slide >
     <slot></slot>
 </aside>
-
+{/if}
